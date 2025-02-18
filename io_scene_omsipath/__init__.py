f43082ad8b5ecae2f7f39654eb78989914d986c1
James Panaligan <contact@jem-suu.me>
2025-02-18 19:27:30 +0000

    Add GPLv2 (will change to GPLv3) and remove testing tag

--- io_scene_omsipath/__init__.py
+++ io_scene_omsipath/__init__.py
@@ -1,3 +1,21 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
 import bpy
 from bpy.props import StringProperty
 from bpy_extras.io_utils import ImportHelper, ExportHelper
@@ -8,13 +26,12 @@ bl_info = {
     "name": "OMSI Pax Configuration File Exporter",
     "description": "Import-Export passenger configuration files for OMSI.",
     "author": "Fabian S., jem_suu",
-    "version": (1, 0, 0),
+    "version": (0, 1, 0),
     "blender": (2, 80, 0),
     "location": "File > Import/Export > OMSI Passenger/Path Config (.cfg)",
     "warning": "", # used for warning icon and text in addons panel
     "doc_url": "https://github.com/jem-suu/io_scene_omsipath",
     "tracker_url": "https://github.com/jem-suu/io_scene_omsipath/issues",
-    "support": "TESTING",
     "category": "Import-Export",
 }
 
@@ -30,7 +47,7 @@ class ImportPaths(bpy.types.Operator, ImportHelper):
         paths.path_import(self.filepath, context)
         return {'FINISHED'}
 
-def menu_func_importpaths(self, context):
+def menu_func_importPaths(self, context):
     self.layout.operator(ImportPaths.bl_idname, text="OMSI Path Config (.cfg)")
 
 # Path Export
@@ -51,7 +68,7 @@ class ExportPaths(bpy.types.Operator, ExportHelper):
             raise Exception(errors)
         return {'FINISHED'}
 
-def menu_func_exportpaths(self, context):
+def menu_func_exportPaths(self, context):
     self.layout.operator(ExportPaths.bl_idname, text="OMSI Path Config (.cfg)")
 
 # PassengerCabin Export
@@ -61,14 +78,14 @@ def menu_func_exportpaths(self, context):
 def register():
     bpy.utils.register_class(ImportPaths)
     bpy.utils.register_class(ExportPaths)
-    bpy.types.TOPBAR_MT_file_import.append(menu_func_importpaths)
-    bpy.types.TOPBAR_MT_file_export.append(menu_func_exportpaths)
+    bpy.types.TOPBAR_MT_file_import.append(menu_func_importPaths)
+    bpy.types.TOPBAR_MT_file_export.append(menu_func_exportPaths)
 
 def unregister():
     bpy.utils.unregister_class(ImportPaths)
     bpy.utils.unregister_class(ExportPaths)
-    bpy.types.TOPBAR_MT_file_import.remove(menu_func_importpaths)
-    bpy.types.TOPBAR_MT_file_export.remove(menu_func_exportpaths)
+    bpy.types.TOPBAR_MT_file_import.remove(menu_func_importPaths)
+    bpy.types.TOPBAR_MT_file_export.remove(menu_func_exportPaths)
 
 if __name__ == "__main__":
     register()
